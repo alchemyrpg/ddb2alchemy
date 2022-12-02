a15d0a7765014f6313fd7f893319eba5591b38fe
Nick Budak <budak@stanford.edu>
2022-12-02 15:15:31 -0800

    Create HTML page skeleton

---
+++ public/index.js
@@ -0,0 +1,1 @@
+(()=>{function a(){let r=document.getElementById("characters");for(let t of r.files){let e=new FileReader;e.onload=()=>{let o=JSON.parse(e.result);console.log(o)},e.readAsText(t)}}window.convert=a;})();
