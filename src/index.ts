18d7b2a4a29368c547690fc4cbd29ec2811f095f
Nick Budak <budak@stanford.edu>
2022-12-02 15:16:09 -0800

    Add file uploader logic

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { convertCharacter } from './convert'
+
+function convert(): void {
+  const uploadInput = document.getElementById("characters") as HTMLInputElement
+  for (const file of uploadInput.files) {
+    const reader = new FileReader()
+    reader.onload = () => {
+      const ddbCharacter = JSON.parse(reader.result as string)
+      console.log(convertCharacter(ddbCharacter))
+    }
+    reader.readAsText(file)
+  }
+}
+
+(window as any).convert = convert
