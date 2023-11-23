7ce63e521c89b06dc21d03a7af32c1e9b2c1a0ad
Isaac Overacker <isaac@alchemyrpg.com>
2023-11-23 15:56:29 -0800

    🧪 Update exp test to expect trackers

--- test/convert.exp.test.ts
+++ test/convert.exp.test.ts
@@ -1,25 +1,30 @@
 import { describe, expect, test } from '@jest/globals';
 import { convertCharacter } from '../src';
+import { AlchemyTrackerCategory } from '../src/alchemy.d';
 import { DdbCharacter } from '../src/ddb';
 import { DeepPartial } from './test-helpers';
 
-describe('Convert DDB currentXp to Alchemy exp', () => {
+describe('Convert DDB currentXp to Alchemy tracker', () => {
     test.each`
         currentXp | expected
         ${10}     | ${10}
         ${0}      | ${0}
     `(
-        'returns exp=$expected when currentXp=$currentXp',
+        'returns tracker.current=$expected when currentXp=$currentXp',
         ({ currentXp, expected }) => {
             const ddbChar: DeepPartial<DdbCharacter> = {
                 currentXp,
             };
 
             const converted = convertCharacter(ddbChar as DdbCharacter, {
-                exp: true,
+                trackers: true,
             });
 
-            expect(converted.exp).toEqual(expected);
+            expect(
+                converted.trackers?.find(
+                    (t) => t.category === AlchemyTrackerCategory.Experience,
+                )?.value,
+            ).toEqual(expected);
         },
     );
 });
